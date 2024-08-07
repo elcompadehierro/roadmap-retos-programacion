e08528d3e704b60140cea9dc1f168c7e3789bae1
elcompadehierro <reydelaspelotas2@outlook.com>
2024-08-06 18:55:53 -0600

    Create elcompadehierro.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/elcompadehierro.py
@@ -0,0 +1,8 @@
+# https://www.python.org/
+"""TOMAAAAAA PERROOOOO"""
+"""COMEEE"""
+int(input ("Enter your favourite score")) = A
+float(input("Enter a radnom number") = B
+int(sum_variable) = A + B
+print("The result of your operation is str(int(sum_variable))
+print("HELLO PYTHON")
